2a90bb42aa4a64634a92364988a82b7dd65faec2
Miles Crabill <miles@milescrabill.com>
2013-09-26 11:14:34 -0700

    Added perimeter function

--- qdmlib.c
+++ qdmlib.c
@@ -154,3 +154,16 @@ void print_x_y(double *x, double *y, int n){
     }
 }
 
+double perimeter(double* x, double* y, int n) {
+	double perim = 0;
+	int j;
+
+	for(int i=0; i<n; i++) {
+	  j = (i+1)%n ;
+		perim += sqrt(pow(x[i]-x[j], 2)+pow(y[i]-y[j], 2));
+
+		printf("Perim: %f\n", perim);
+	}
+	printf("---\n");
+	return perim;
+}
