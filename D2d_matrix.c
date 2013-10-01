24a6147ea7e8e7531f9acaaa23f1f124f4e446c4
Quinn Rohlf <qr@qrohlf.com>
2013-10-01 11:54:31 -0700

    matrix multiplying worksacom D2d_matrix_test02.c && ./a.out

--- D2d_matrix.c
+++ D2d_matrix.c
@@ -3,7 +3,10 @@
 
 
 int D2d_print_mat (double a[3][3]) {
-    return 0; //TODO: implement this function and remove this comment
+    for (int i = 0; i < 3; i++) {
+        printf("%.0f %.0f %.0f \n", a[i][0], a[i][1], a[i][2]);
+    }
+    return 0;
 }
 
 /* Copy the contents of b into a */
@@ -16,11 +19,26 @@ int D2d_copy_mat (double a[3][3], double b[3][3]) {
     return 0; //What are we supposed to return here?
 }
 
+/* DANGER WILL ROBINSON
+ * this overwrites b with a transposed version.
+ */
 int D2d_mat_mult (double res[3][3], double a[3][3], double b[3][3]) {
-    return 0; //TODO: implement this function and remove this comment
+    double B[3][3];
+    D2d_transpose(b, B);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            res[i][j] = D2d_dot(a[i], B[j]);
+        }
+    }
+    return 0; //What are we supposed to return here?
 }
 
 int D2d_make_identity (double a[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            a[i][j] = (i == j);
+        }
+    }
     return 0; //TODO: implement this function and remove this comment
 }
 
@@ -51,21 +69,18 @@ int D2d_mat_mult_points (double *X, double *Y,
     return 0; //TODO: implement this function and remove this comment
 }
 
-int D2d_transpose (double a[3][3]) {
-    double b[3][3];
+/* 
+ * Transpose a into b
+ */
+int D2d_transpose (double a[3][3], double b[3][3]) {
     for (int i = 0; i < 3; i++) {
         for(int j=0; j < 3; j++) {
             b[i][j] = a[j][i];
         }
     }
-    D2d_copy_mat(a, b);
     return 0; //What are we supposed to return here?
 }
 
-int D2d_dot (double a[3], double b[3]) {
-    double dotprod = 0;
-    for (int i=0; i<3; i++) {
-        dotprod += a[i] * b[i];
-    }
-    return dotprod;
+double D2d_dot (double a[3], double b[3]) {
+    return a[0]*b[0]+a[1]*b[1]+a[2]*b[2];
 }
